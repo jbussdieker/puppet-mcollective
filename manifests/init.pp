d45beafaf481e5afd89a6940be5353ba8a239588
Joshua Bussdieker <jbussdieker@gmail.com>
2013-07-16 10:44:22 -0700

    Fix issue with ruby 1.8.7/puppet 2.6.11

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,7 +3,7 @@ class mcollective(
   $connector_host     = "localhost",
   $connector_port     = 61613,
   $connector_user     = "mcollective",
-  $connector_password = "secret",
+  $connector_password = "secret"
 ) {
 
   class {'mcollective::package':
