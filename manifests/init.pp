06da923ccddaab6d1ad0eee9fe599810335ec849
Joshua B. Bussdieker <jbussdieker@brightroll.com>
2015-03-16 09:29:55 -0700

    Change default connector to activemq to keep with the times

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,11 @@
 # == Class: mcollective
 class mcollective(
   $client             = false,
-  $connector          = 'stomp',
-  $connector_host     = 'localhost',
-  $connector_port     = 61613,
+  $connector          = 'activemq',
+  $connector_host     = 'stomp1',
+  $connector_port     = 6163,
   $connector_user     = 'mcollective',
-  $connector_password = 'secret'
+  $connector_password = 'marionette'
 ) {
 
   class {'mcollective::package':
